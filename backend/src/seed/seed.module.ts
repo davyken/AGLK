7e65c6b090736469f17f2a46c6f7e31d0a6f0d16
davyken <davykennang552@gmail.com>
2026-04-07 16:30:43 +0100

    fixed deployment issues from render

--- backend/src/seed/seed.module.ts
+++ backend/src/seed/seed.module.ts
@@ -1,8 +1,13 @@
 import { Module, Global } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { SeedService } from './seed.service';
+import { User, UserSchema } from '../common/schemas/user.schema';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [SeedService],
   exports: [SeedService],
 })
